4a15d0723dfea88022ffb86b8de455c10e8f714c
Cody Winton <cody.t.winton@gmail.com>
2018-03-04 16:15:22 -0600

    Re-added project files

---
+++ SwiftCommonMark/SwiftCommonMark.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCommonMark.h
+//  SwiftCommonMark
+//
+//  Created by Cody Winton on 3/4/18.
+//  Copyright © 2018 Cody Winton. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftCommonMark.
+FOUNDATION_EXPORT double SwiftCommonMarkVersionNumber;
+
+//! Project version string for SwiftCommonMark.
+FOUNDATION_EXPORT const unsigned char SwiftCommonMarkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCommonMark/PublicHeader.h>
+
+
