8c89a74b675bdf82abbed6d59986385f50def351
Cody Winton <cody.t.winton@gmail.com>
2018-03-09 17:06:19 -0600

    Improved output for CommonMark

--- Sources/CommonMarkOutputable.swift
+++ Sources/CommonMarkOutputable.swift
@@ -15,3 +15,51 @@ import Foundation
 protocol CommonMarkOutputable {
 	var commonMark: String { get }
 }
+
+// MARK: - Extensions
+
+// MARK: - CommonMark Output Extensions
+
+extension Node: CommonMarkOutputable {
+
+	var commonMark: String {
+		switch self {
+		case .thematicBreak:
+			return "***"
+		case .document(let nodes), .paragraph(let nodes):
+			return nodes.commonMark
+		case .text(let str), .softBreak(let str), .lineBreak(let str), .htmlInline(let str), .htmlBlock(let str):
+			return str
+		case .code(let code):
+			return "`" + code + "`"
+		case .strong(let nodes):
+			return "**" + nodes.commonMark + "**"
+		case .emphasis(let nodes):
+			return "*" + nodes.commonMark + "*"
+		case let .heading(level, nodes):
+			return String(repeating: "#", count: level.rawValue) + " " + nodes.commonMark
+		case let .codeBlock(info, code):
+			let lang = info ?? ""
+			return "```\(lang)" + code + "```"
+		case let .image(source, title, alternate):
+			let alt = alternate ?? ""
+			var srcTitle = ""
+
+			if let title = title {
+				srcTitle += " " + title
+			}
+
+			return "![\(alt)](\(source)\(srcTitle))"
+		case .blockQuote, .list, .item,
+			 .link, .customInline, .customBlock:
+			return ""
+		}
+	}
+}
+
+extension Array where Element: CommonMarkOutputable {
+
+	var commonMark: String {
+		return map { $0.commonMark }.joined()
+	}
+}
