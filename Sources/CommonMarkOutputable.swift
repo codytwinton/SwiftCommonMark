286cc2ad8b2755f1b6d1420b854b3d6774d5a22e
Cody Winton <cody.t.winton@gmail.com>
2018-03-09 17:04:22 -0600

    Added CommonMarkOutputable

---
+++ Sources/CommonMarkOutputable.swift
@@ -0,0 +1,17 @@
+//
+//  CommonMarkOutputable.swift
+//  SwiftCommonMark
+//
+//  Created by Cody Winton on 3/8/18.
+//  Copyright © 2018 Cody Winton. All rights reserved.
+//
+
+// MARK: Imports
+
+import Foundation
+
+// MARK: - Protocols
+
+protocol CommonMarkOutputable {
+	var commonMark: String { get }
+}
