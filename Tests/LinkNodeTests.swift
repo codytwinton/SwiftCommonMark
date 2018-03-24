d24f94a31db8727692f76bea200c7590fc686e3d
Cody Winton <cody.t.winton@gmail.com>
2018-03-24 16:52:39 -0500

    Added LinkNode Tests

---
+++ Tests/LinkNodeTests.swift
@@ -0,0 +1,57 @@
+//
+//  LinkNodeTests.swift
+//  SwiftCommonMarkTests
+//
+//  Created by Cody Winton on 3/24/18.
+//  Copyright © 2018 Cody Winton. All rights reserved.
+//
+
+// MARK: Imports
+
+@testable import SwiftCommonMark
+import XCTest
+
+// MARK: -
+
+class LinkNodeTests: XCTestCase {
+
+	let node1: LinkNode = LinkNode(url: "/uri", title: "title", nodes: [TextNode("link")])
+	let node2: LinkNode = LinkNode(url: "/uri", title: nil, nodes: [])
+
+	func testTypes() {
+		XCTAssertEqual(node1.type, .link)
+		XCTAssertEqual(node2.type, .link)
+	}
+
+	func testHTML() {
+		let expecteds = ["<a href=\"/uri\" title=\"title\">link</a>",
+						 "<a href=\"/uri\"></a>"]
+
+		for (i, node) in [node1, node2].enumerated() {
+			let actual = node.html
+			let expected = expecteds[i]
+
+			XCTAssertEqual(expected, actual, "\n\n\n\n********\n\n" +
+				"Failed HTML:" +
+				"\n\nExpected: |\(expected)|" +
+				"\n\nActual: |\(actual)|" +
+				"\n********\n\n\n\n\n\n")
+		}
+	}
+
+	func testCommonMark() {
+
+		let expecteds = ["[link](/uri \"title\")", "[](/uri)"]
+
+		for (i, node) in [node1, node2].enumerated() {
+			let actual = node.commonMark
+			let expected = expecteds[i]
+
+			XCTAssertEqual(expected, actual, "\n\n\n\n********\n\n" +
+				"Failed CommonMark:" +
+				"\n\nExpected: |\(expected)|" +
+				"\n\nActual: |\(actual)|" +
+				"\n********\n\n\n\n\n\n")
+		}
+	}
+}
