cecf4e1dee3ae6db66a0e5410f2cac0159e6de59
Cody Winton <cody.t.winton@gmail.com>
2018-03-24 16:58:44 -0500

    Added ListItemNodeTests

---
+++ Tests/ListItemNodeTests.swift
@@ -0,0 +1,49 @@
+//
+//  ListItemNodeTests.swift
+//  SwiftCommonMarkTests
+//
+//  Created by Cody Winton on 3/24/18.
+//  Copyright © 2018 Cody Winton. All rights reserved.
+//
+
+// MARK: Imports
+
+@testable import SwiftCommonMark
+import XCTest
+
+// MARK: -
+
+class ListItemNodeTests: XCTestCase {
+
+	// MARK: Constants
+
+	let node: ListItemNode = ListItemNode(nodes: [ParagraphNode(nodes: [TextNode("List")])])
+
+	// MARK: HeadingNode Tests
+
+	func testTypes() {
+		XCTAssertEqual(node.type, .listItem)
+	}
+
+	func testHTML() {
+		let actual = node.html
+		let expected = "<li><p>List</p>\n</li>\n"
+
+		XCTAssertEqual(expected, actual, "\n\n\n\n********\n\n" +
+			"Failed HTML:" +
+			"\n\nExpected: |\(expected)|" +
+			"\n\nActual: |\(actual)|" +
+			"\n********\n\n\n\n\n\n")
+	}
+
+	func testCommonMark() {
+		let actual = node.commonMark
+		let expected = "List\n\n\n"
+
+		XCTAssertEqual(expected, actual, "\n\n\n\n********\n\n" +
+			"Failed CommonMark:" +
+			"\n\nExpected: |\(expected)|" +
+			"\n\nActual: |\(actual)|" +
+			"\n********\n\n\n\n\n\n")
+	}
+}
