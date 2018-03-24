85ae8e3f3b15595b31f50f89249055cd2e837921
Cody Winton <cody.t.winton@gmail.com>
2018-03-24 17:31:39 -0500

    ParagraphNode Tests

---
+++ Tests/ParagraphNodeTests.swift
@@ -0,0 +1,56 @@
+//
+//  ParagraphNodeTests.swift
+//  SwiftCommonMarkTests
+//
+//  Created by Cody Winton on 3/24/18.
+//  Copyright © 2018 Cody Winton. All rights reserved.
+//
+
+// MARK: Imports
+
+@testable import SwiftCommonMark
+import XCTest
+
+// MARK: -
+
+class ParagraphNodeTests: XCTestCase {
+
+	// MARK: Constants
+
+	let node: ParagraphNode = ParagraphNode(nodes: [TextNode("Testing Paragraph")])
+
+	// MARK: HeadingNode Tests
+
+	func testTypes() {
+		XCTAssertEqual(node.type, .paragraph)
+	}
+
+	func testHTML() {
+		let actual = node.html
+		let expected = """
+			<p>Testing Paragraph</p>
+
+			"""
+
+		XCTAssertEqual(expected, actual, "\n\n\n\n********\n\n" +
+			"Failed HTML:" +
+			"\n\nExpected: |\(expected)|" +
+			"\n\nActual: |\(actual)|" +
+			"\n********\n\n\n\n\n\n")
+	}
+
+	func testCommonMark() {
+		let actual = node.commonMark
+		let expected = """
+			Testing Paragraph
+
+
+			"""
+
+		XCTAssertEqual(expected, actual, "\n\n\n\n********\n\n" +
+			"Failed CommonMark:" +
+			"\n\nExpected: |\(expected)|" +
+			"\n\nActual: |\(actual)|" +
+			"\n********\n\n\n\n\n\n")
+	}
+}
