4f61bfe98ecce5a1960e91880d7a09fa6777b362
Cody Winton <cody.t.winton@gmail.com>
2018-03-26 18:53:18 -0500

    Migrated old parsing code

--- Sources/CommonMarkParser.swift
+++ Sources/CommonMarkParser.swift
@@ -14,38 +14,114 @@ import Foundation
 
 extension NodeType {
 
-	// MARK: Static Variables
-
-	static var blockTypes: [NodeType] {
-		return [.codeBlock, .blockQuote, .heading, .thematicBreak, .list, .htmlBlock, .paragraph]
-	}
-
 	// MARK: Variables
 
-	var blockRegex: NSRegularExpression? {
+	var regex: NSRegularExpression? {
 		let pattern: String
+		let options: NSRegularExpression.Options = .anchorsMatchLines
 
 		switch self {
-		case .thematicBreak:
-			pattern = "^(?:(?:[ ]{0,3}\\*[ \t]*){3,}|(?:[ ]{0,3}_[ \t]*){3,}|(?:[ ]{0,3}-[ \t]*){3,})[ \t]*$"
+		case .code:
+			pattern = "(\\`+)([^\\`]{1}[\\s\\S\\\\]*?[^\\`]*)\\1"
+		case .emphasis:
+			pattern = "([*_]{1})([\\w(]+.*[\\w)]+)(\\1)"
 		case .heading:
 			pattern = "^ {0,3}(#{1,6})(?:[ \t]+|$)(.*)"
-		case .blockQuote, .code, .codeBlock, .document, .emphasis, .htmlBlock, .htmlInline,
-			 .image, .listItem, .lineBreak, .link, .list, .paragraph, .softBreak, .strong, .text:
+		case .strong:
+			pattern = "([*_]{2})([\\w(]+.*[\\w)]+)(\\1)"
+		case .thematicBreak:
+			pattern = "^(?:(?:[ ]{0,3}\\*[ \t]*){3,}|(?:[ ]{0,3}_[ \t]*){3,}|(?:[ ]{0,3}-[ \t]*){3,})[ \t]*$"
+		case .blockQuote, .codeBlock, .document, .htmlBlock, .htmlInline,
+			 .image, .listItem, .lineBreak, .link, .list, .paragraph, .softBreak, .text:
 			return nil
 		}
 
-		return try? NSRegularExpression(pattern: pattern, options: .anchorsMatchLines)
+		return try? NSRegularExpression(pattern: pattern, options: options)
 	}
 
-	var blockTemplates: [String] {
+	var regexTemplates: [String] {
 		switch self {
 		case .heading:
 			return ["$1", "$2"]
-		case .blockQuote, .code, .codeBlock, .document, .emphasis, .htmlBlock,
+		case .strong, .emphasis, .code:
+			return ["$2"]
+		case .blockQuote, .codeBlock, .document, .htmlBlock,
 			 .htmlInline, .image, .listItem, .lineBreak, .link, .list,
-			 .paragraph, .softBreak, .strong, .text, .thematicBreak:
+			 .paragraph, .softBreak, .text, .thematicBreak:
 			return []
+		}
+	}
+
+	// MARK: Functions
+
+	func parse(markdown: String) -> [Node] {
+
+		var nodes: [Node] = []
+		guard !markdown.isEmpty else { return nodes }
+
+		var input = markdown
+
+		while !input.isEmpty {
+
+			var isMatched = false
+
+			for child in childNodes {
+				guard let regexMatch = child.regex?.match(in: input, templates: child.regexTemplates) else { continue }
+				let result = child.node(from: regexMatch.captures)
+				nodes.append(result)
+
+				let inputOffset = input.index(input.startIndex, offsetBy: regexMatch.fullMatch.count)
+				input = String(input[inputOffset...])
+				isMatched = true
+				break
+			}
+
+			guard !isMatched else { continue }
+
+			if input.first == "\n" {
+				// Ignore Newlines Characters
+				input.removeFirst()
+			} else if self == .document, let paragraphIndex = input.index(of: "\n") {
+				let paragraph = String(input[..<paragraphIndex])
+				let paragraphNodes = NodeType.paragraph.parse(markdown: paragraph)
+				nodes.append(.paragraph(nodes: paragraphNodes))
+				input = String(input[paragraphIndex...])
+			} else {
+				nodes.append(.text(input))
+				input = ""
+			}
+		}
+
+		return nodes
+	}
+
+	func node(from matches: [String]) -> Node {
+		switch self {
+		case .code:
+			return .code(matches[0])
+		case .thematicBreak:
+			return .thematicBreak
+		case .heading:
+			let level: HeadingLevel = HeadingLevel(rawValue: matches[0].count) ?? .h1
+			let raw = matches[1]
+			var components = raw.components(separatedBy: " #")
+
+			if let last = components.last?.trimmingCharacters(in: .whitespaces) {
+				let set = Set(last)
+				if set.count == 1, set.first == "#" {
+					components.removeLast()
+				}
+			}
+
+			let markdown = components.joined(separator: " #").replacingOccurrences(of: "\\#", with: "#")
+			return .heading(level: level, nodes: NodeType.heading.parse(markdown: markdown))
+		case .strong:
+			return .strong(nodes: NodeType.strong.parse(markdown: matches[0]))
+		case .emphasis:
+			return .emphasis(nodes: NodeType.emphasis.parse(markdown: matches[0]))
+		case .document, .blockQuote, .codeBlock, .htmlBlock, .htmlInline,
+			 .image, .listItem, .lineBreak, .link, .list, .paragraph, .softBreak, .text:
+			return .text("")
 		}
 	}
 
