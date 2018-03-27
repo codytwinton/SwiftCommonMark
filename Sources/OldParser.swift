ec818b38c4b2a831c385b14b3dec10f4964c384b
Cody Winton <cody.t.winton@gmail.com>
2018-03-26 21:51:56 -0500

    Removed more unnecessary code

--- Sources/OldParser.swift
+++ Sources/OldParser.swift
@@ -16,63 +16,6 @@ extension NodeType {
 
 	// MARK: Functions
 
-	func parse(markdown: String) -> [Node] {
-
-		var nodes: [Node] = []
-		guard !markdown.isEmpty else { return nodes }
-
-		var input = markdown
-
-		while !input.isEmpty {
-
-			var isMatched = false
-
-			for child in childNodes {
-				guard let regexMatch = child.regex?.match(in: input, templates: child.regexTemplates) else { continue }
-				let result = child.node(from: regexMatch.captures)
-				nodes.append(result)
-
-				let inputOffset = input.index(input.startIndex, offsetBy: regexMatch.fullMatch.count)
-				input = String(input[inputOffset...])
-				isMatched = true
-				break
-			}
-
-			guard !isMatched else { continue }
-
-			if input.first == "\n" {
-				// Ignore Newlines Characters
-				input.removeFirst()
-			} else if self == .document, let paragraphIndex = input.index(of: "\n") {
-				let paragraph = String(input[..<paragraphIndex])
-				let paragraphNodes = NodeType.paragraph.parse(markdown: paragraph)
-				nodes.append(.paragraph(nodes: paragraphNodes))
-				input = String(input[paragraphIndex...])
-			} else {
-				nodes.append(.text(input))
-				input = ""
-			}
-		}
-
-		return nodes
-	}
-
-	func node(from matches: [String]) -> Node {
-		switch self {
-		case .code:
-			return .code(matches[0])
-		case .strong:
-			return .strong(nodes: NodeType.strong.parse(markdown: matches[0]))
-		case .emphasis:
-			return .emphasis(nodes: NodeType.emphasis.parse(markdown: matches[0]))
-		case .document, .blockQuote, .codeBlock, .htmlBlock, .htmlInline,
-			 .image, .listItem, .lineBreak, .link, .list, .paragraph, .softBreak, .text:
-			return .text("")
-		}
-	}
-
-	// MARK: -
-
 	static func parse(markdown: String) -> Node {
 
 		var blocks: [Node] = []
